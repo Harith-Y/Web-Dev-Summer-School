cf3630b08cfbd665ffae0e3bc480f24e32c17791
Harith Yerragolam <yharith16@gmail.com>
2024-07-06 19:17:13 +0530

    Task 8 completed

---
+++ Task 8/BlogAPI/server.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+
+//Connecting to Mongoose
+mongoose
+  .connect(process.env.DATABASE_URL)
+  .then(() => console.log("Connected"))
+  .catch((err) => console.log(err));
+
+//Middleware
+app.use(express.json());
+
+// Task routes
+const tasksRouter = require("./routes/Tasks");
+app.use("/tasks", tasksRouter);
+
+//Blog routes
+const blogsRouter = require("./routes/blogs");
+app.use("/blogs", blogsRouter);
+
+//Listem to server
+app.listen(3000, () => console.log("Server connected"));
